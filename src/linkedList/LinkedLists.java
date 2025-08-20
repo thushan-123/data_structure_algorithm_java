e6eb3aac75f862479c726ef1edcfa5a0facea48e
thushan-123 <thushanmadusanka456@gmail.com>
2025-08-20 19:43:36 +0530

    create linkedList scratch

---
+++ src/linkedList/LinkedLists.java
@@ -0,0 +1,109 @@
+package linkedList;
+
+public class LinkedLists {
+
+    // linkedList methods  add(), search(), remove(), printAll()
+
+    // LinkedList stores 2 parts in Node (data , address)
+
+    //
+    //
+    // Advantages : Dynamic data structure (allocate memory while run time)
+    //              Easy to Insertion and Deletion  O(1)
+    //              Low memory waste
+
+    // Disadvantages : LinkedList slower than Arrays because LinkedList allocate memory different memory location
+    //                 Arrays allocate the memory in nearest memory location, its elements can access fast
+
+
+    private Node head = null;
+    private Node tail = null;
+
+    private int size = 0;
+
+    public String add(String name){
+        Node newNode = new Node(name);
+        if(head == null && tail == null){
+            // linkedList first element , create a new Node object
+            newNode.next = null;
+            head = newNode;
+            tail = newNode;
+            size +=1;
+
+        }else{
+            newNode.next =tail;
+            tail=newNode;
+            size +=1;
+
+        }
+        return name;
+    }
+
+    public void printAll(){
+        System.out.println("LinkedList elements : ");
+        if(head == null){
+            System.out.println("no element in linkedList");
+        }else{
+            Node tmp = tail;
+            while(tmp != null){
+                System.out.println(tmp.name);
+                tmp = tmp.next;
+            }
+        }
+    }
+
+    public int searchIndex(String name){
+        if(head == null){
+            // if linkList is empty , return the value -1
+            return -1;
+        }else{
+            Node temp = tail;
+            int i = 0;
+            while(temp != null){
+                if (temp.name.equals(name)){
+                    return size -i -1;
+                }
+                temp = temp.next;
+                i +=1;
+            }
+            return -1; // if element is not found return -1
+        }
+    }
+
+    public int numOfElement(){
+        return size;
+    }
+
+    public boolean remove(String name){
+        if(tail.name.equals(name)){
+            // this is last object  -> change tail.next
+            tail = tail.next;
+            return true;
+        }else{
+            // find the value
+            int i = 0;
+            Node tmp = tail;
+            while (i < size){
+                if(tmp.next.name.equals(name)){
+
+                    // we should remove the next object (temp.next)
+                    tmp.next = tmp.next.next;
+
+                    return true;
+                }else{
+                    i +=1;
+                    tmp = tmp.next;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean insert(String name, int index){
+        System.out.println("");
+        return true;
+    }
+
+
+
+}
---
+++ src/linkedList/Node.java
@@ -0,0 +1,13 @@
+package linkedList;
+
+public class Node {
+    protected String name;
+    protected Node next;
+
+    public Node(String name){
+        this.name = name;
+        this.next = null;
+    }
+
+
+}
