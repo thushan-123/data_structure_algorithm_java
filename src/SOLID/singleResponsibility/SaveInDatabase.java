18b0fd7cfe2a863e36bfa2cd4df6c9915ba4b043
thushan-123 <thushanmadusanka456@gmail.com>
2025-08-22 01:45:18 +0530

    create separete fn save db

---
+++ src/SOLID/singleResponsibility/SaveInDatabase.java
@@ -0,0 +1,8 @@
+package SOLID.singleResponsibility;
+
+// create separate class for save to data in database
+public class SaveInDatabase {
+    public boolean saveEmployeeInDatabase(Employee employee){
+        return true;
+    }
+}
