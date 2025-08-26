f44d41a81af0a11fe9c242c94ee917da72a58a8f
thushan-123 <thushanmadusanka456@gmail.com>
2025-08-26 16:38:28 +0530

    create tree node

---
+++ src/tree/Node.java
@@ -0,0 +1,11 @@
+package tree;
+
+public class Node {
+    public int data;
+    public Node leftChild = null;
+    public Node rightChile = null;
+
+    public Node(int data){
+        this.data = data;
+    }
+}
