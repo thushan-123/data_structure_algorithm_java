f83d7f66babaf4b0e6296c9086de2a39e4ca7464
thushan-123 <thushanmadusanka456@gmail.com>
2025-08-26 17:18:00 +0530

    add values check

---
+++ src/tree/Main.java
@@ -0,0 +1,14 @@
+package tree;
+
+public class Main {
+    public static void main(String[] args) {
+        BinaryTree binaryTree = new BinaryTree();
+        binaryTree.addNode(10);
+        binaryTree.addNode(8);
+        binaryTree.addNode(18);
+        binaryTree.addNode(17);
+        binaryTree.addNode(2);
+
+        System.out.println("add data success");
+    }
+}
