e6b5d3fecf4c584cc296a004e3aa55a4b68c3802
thushan-123 <thushanmadusanka456@gmail.com>
2025-08-26 17:10:26 +0530

    implement addnode fn

--- src/tree/BinaryTree.java
+++ src/tree/BinaryTree.java
@@ -4,13 +4,36 @@ public class BinaryTree {
 
     Node rootNode;
 
-    public boolean addNode(int value){
+    public BinaryTree(){}
+
+    public void addNode(int value){
         // first create new node
         Node newNode = new Node(value);
 
         // check root node is null
         if(rootNode == null){
-
+            rootNode = newNode;
+            return;
         }
+
+        Node tmpNode = rootNode;
+        while (true){
+
+            if(tmpNode.data < value){
+                // left side node
+                if (tmpNode.leftChild == null){
+                    tmpNode.leftChild = newNode;
+                    break;
+                }
+                continue;
+            }else{
+                if(tmpNode.rightChild == null){
+                    tmpNode.rightChild = newNode;
+                    break;
+                }
+                continue;
+            }
+        }
+
     }
 }
