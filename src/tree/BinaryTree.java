f92a6c411d9944758643563ee5b5084e5a2a58a7
thushan-123 <thushanmadusanka456@gmail.com>
2025-08-26 16:48:15 +0530

    refactot tree to binarytree class name

---
+++ src/tree/BinaryTree.java
@@ -0,0 +1,16 @@
+package tree;
+
+public class BinaryTree {
+
+    Node rootNode;
+
+    public boolean addNode(int value){
+        // first create new node
+        Node newNode = new Node(value);
+
+        // check root node is null
+        if(rootNode == null){
+
+        }
+    }
+}
