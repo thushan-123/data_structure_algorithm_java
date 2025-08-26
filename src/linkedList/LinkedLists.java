ca4a8b6dd8948ee53df65798c44f0f86657ffb77
thushan-123 <thushanmadusanka456@gmail.com>
2025-08-26 16:35:19 +0530

    add exception

--- src/linkedList/LinkedLists.java
+++ src/linkedList/LinkedLists.java
@@ -118,9 +118,9 @@ public class LinkedLists {
     }
 
     public void delete(int index){
-//        if(size < index){
-//            throw new IndexOutOfBoundsException();
-//        }
+        if(size < index){
+            throw new IndexOutOfBoundsException();
+        }
 
         // check last index
         if(size -1 == index){
