5a3ecf7d178ccde08d083c66e0f24a68bdd2db72
thushan-123 <thushanmadusanka456@gmail.com>
2025-08-26 17:09:49 +0530

    rename class attr

--- src/tree/Node.java
+++ src/tree/Node.java
@@ -3,7 +3,7 @@ package tree;
 public class Node {
     public int data;
     public Node leftChild = null;
-    public Node rightChile = null;
+    public Node rightChild = null;
 
     public Node(int data){
         this.data = data;
